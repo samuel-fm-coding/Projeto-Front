880be8c608cb461ff0ec2dbd9580618beed218eb
samuel-fm-coding <samuelfernandescor@gmail.com>
2024-09-11 21:04:06 -0300

    first commit

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
     e.preventDefault();
     returnModalLogin();
   });
+  $('#cpfCadastro').mask('000.000.000-00');
 });
 
 
@@ -15,11 +16,11 @@ function returnHeader(){
         <img src="https://th.bing.com/th/id/R.421fd445b32643f4e1ab54216f80f29c?rik=ok5b86CfpNIYKQ&pid=ImgRaw&r=0" width="75" alt="Logo">
       </div>
       <div class="col-8 text-right">
-        <a href="#">Home</a>
+        <a href="/">Home</a>
         <a href="#">Alugar</a>
         <a href="#">Anunciar</a>
         <a href="#">A LocaHouse</a>
-        <button type="button" class="btn btn-primary" id="btn-criar-conta">Criar conta</button>
+        <a href="cadastro.html" class="btn btn-primary" id="btn-criar-conta">Criar conta</a>
         <button type="button" class="btn btn-outline-primary" id="btn-login"><i class="bi bi-person-circle"></i> Entrar</button>
       </div>
     </div>`;
