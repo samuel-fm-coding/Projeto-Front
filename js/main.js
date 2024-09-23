975a53bd409402b36be21b07b0e5e295922cb55c
samuel-fm-coding <samuelfernandescor@gmail.com>
2024-09-23 13:23:45 +0000

    index e ajustes

--- js/main.js
+++ js/main.js
@@ -1,10 +1,6 @@
 $(document).ready(function(){
   returnHeader();
   
-  $("#btn-login").off('click').on('click', function(e){
-    e.preventDefault();
-    returnModalLogin();
-  });
   $('#cpfCadastro').mask('000.000.000-00');
 });
 
@@ -12,53 +8,19 @@ $(document).ready(function(){
 
 function returnHeader(){
   var html = `<div class="row align-items-center">
-      <div class="col-4">
-        <img src="https://th.bing.com/th/id/R.421fd445b32643f4e1ab54216f80f29c?rik=ok5b86CfpNIYKQ&pid=ImgRaw&r=0" width="75" alt="Logo">
-      </div>
-      <div class="col-8 text-right">
-        <a href="/">Home</a>
-        <a href="#">Alugar</a>
-        <a href="#">Anunciar</a>
-        <a href="#">A LocaHouse</a>
-        <a href="cadastro.html" class="btn btn-primary" id="btn-criar-conta">Criar conta</a>
-        <a href="login.html" class="btn btn-outline-primary" id="btn-login"><i class="bi bi-person-circle"></i> Entrar</button>
-      </div>
-    </div>`;
+                <div class="col-4">
+                  <img src="https://th.bing.com/th/id/R.421fd445b32643f4e1ab54216f80f29c?rik=ok5b86CfpNIYKQ&pid=ImgRaw&r=0" width="75" alt="Logo">
+                </div>
+                <div class="col-8 text-right">
+                  <a href="/">Home</a>
+                  <a href="#">Alugar</a>
+                  <a href="#">Anunciar</a>
+                  <a href="#">A LocaHouse</a>
+                  <a href="cadastro.html" class="btn btn-primary" id="btn-criar-conta">Criar conta</a>
+                  <a href="login.html" class="btn btn-outline-primary" id="btn-login"><i class="bi bi-person-circle"></i> Entrar</a>
+                </div>
+              </div>`;
     
     $('header').html(html);
 }
 
-function returnModalLogin(){
-  if ($("#modal-login").length == 0) {
-    var modal = `<div id="modal-login" class="modal fade" role="dialog">
-                   <div class="modal-dialog">
-                     <div class="modal-content">
-                       <div class="modal-header">
-                         <h5 class="modal-title">Fazer Login</h5>
-                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                           <span aria-hidden="true">&times;</span>
-                         </button>
-                       </div>
-                       <div class="modal-body">
-                         <div class="form-group">
-                            <label for="nomeCadastro">CPF</label>
-                            <input type="text" class="form-control" id="cpfLogin" placeholder="Digite o seu CPF">
-                         </div>
-                         <div class="form-group">
-                            <label for="senhaCadastro">Senha</label>
-                            <input type="password" class="form-control" id="senhaCadastro" placeholder="Insira a senha">
-                         </div>
-                       </div>
-                       <div class="modal-footer">
-                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
-                         <button type="button" class="btn btn-primary" id="btn-enviar-login">Enviar</button>
-                       </div>
-                     </div>
-                   </div>
-                 </div>`;
-    $("body").append(modal); // Adiciona o modal ao corpo
-  }
-  
-  // Mostra o modal
-  $("#modal-login").modal('show');
-}
