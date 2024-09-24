50efff050dc7df2fab76411284705aa69a469888
samuel-fm-coding <samuelfernandescor@gmail.com>
2024-09-24 14:19:48 +0000

    nova cor e logo

--- js/main.js
+++ js/main.js
@@ -9,14 +9,14 @@ $(document).ready(function(){
 function returnHeader(){
   var html = `<div class="row align-items-center">
                 <div class="col-4">
-                  <img src="https://th.bing.com/th/id/R.421fd445b32643f4e1ab54216f80f29c?rik=ok5b86CfpNIYKQ&pid=ImgRaw&r=0" width="75" alt="Logo">
+                  <img src="img/logo.jpg" width="125" alt="Logo">
                 </div>
                 <div class="col-8 text-right">
                   <a href="/">Home</a>
                   <a href="#">Alugar</a>
                   <a href="#">Anunciar</a>
                   <a href="#">A LocaHouse</a>
-                  <a href="cadastro.html" class="btn btn-primary" id="btn-criar-conta">Criar conta</a>
+                  <a href="cadastro.html" class="btn" id="btn-criar-conta" style="background-color: #678a73; color: #e4e8e2;">Criar conta</a>
                   <a href="login.html" class="btn btn-outline-primary" id="btn-login"><i class="bi bi-person-circle"></i> Entrar</a>
                 </div>
               </div>`;
