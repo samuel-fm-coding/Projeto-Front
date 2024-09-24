c65ac6e1f53a98ef24d932de54f513b488b616b1
samuel-fm-coding <samuelfernandescor@gmail.com>
2024-09-24 16:23:58 +0000

    listagem

--- js/main.js
+++ js/main.js
@@ -9,15 +9,15 @@ $(document).ready(function(){
 function returnHeader(){
   var html = `<div class="row align-items-center">
                 <div class="col-4">
-                  <img src="img/logo.jpg" width="125" alt="Logo">
+                  <a href="/"><img src="img/logo.jpg" width="150" alt="Logo"></a>
                 </div>
                 <div class="col-8 text-right">
                   <a href="/">Home</a>
-                  <a href="#">Alugar</a>
+                  <a href="imoveis.html">Alugar</a>
                   <a href="#">Anunciar</a>
                   <a href="#">A LocaHouse</a>
-                  <a href="cadastro.html" class="btn" id="btn-criar-conta" style="background-color: #678a73; color: #e4e8e2;">Criar conta</a>
-                  <a href="login.html" class="btn btn-outline-primary" id="btn-login"><i class="bi bi-person-circle"></i> Entrar</a>
+                  <a href="cadastro.html" class="btn btn-back-green" id="btn-criar-conta">Criar conta</a>
+                  <a href="login.html" class="btn btn-outline-lightgreen" id="btn-login"><i class="bi bi-person-circle"></i> Entrar</a>
                 </div>
               </div>`;
     
