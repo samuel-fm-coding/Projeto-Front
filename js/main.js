5e9fd1788f053253423811af70440d9523779f1f
samuel-fm-coding <samuelfernandescor@gmail.com>
2024-09-27 15:11:57 +0000

    ajustes e inicio form cad_imovel

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ function returnHeader(){
                 <div class="col-8 text-right">
                   <a href="/">Home</a>
                   <a href="imoveis.html">Alugar</a>
-                  <a href="#">Anunciar</a>
+                  <a href="cadastrar_imovel.html">Anunciar</a>
                   <a href="#">A LocaHouse</a>
                   <a href="cadastro.html" class="btn btn-back-green" id="btn-criar-conta">Criar conta</a>
                   <a href="login.html" class="btn btn-outline-lightgreen" id="btn-login"><i class="bi bi-person-circle"></i> Entrar</a>
