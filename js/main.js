84725857c7f4c690803d36a0f58f1747b746984a
samuel-fm-coding <samuelfernandescor@gmail.com>
2024-10-09 20:15:53 -0300

    troca do bootstrap

--- js/main.js
+++ js/main.js
@@ -8,18 +8,19 @@ $(document).ready(function(){
 
 function returnHeader(){
   var html = `<div class="row align-items-center">
-                <div class="col-4">
-                  <a href="/"><img src="img/logo.jpg" width="150" alt="Logo"></a>
+                <div class="col-xs-12 col-sm-4">
+                    <a href="/"><img src="img/logo.jpg" width="150" alt="Logo"></a>
                 </div>
-                <div class="col-8 text-right">
-                  <a href="/">Home</a>
-                  <a href="imoveis.html">Alugar</a>
-                  <a href="cadastrar_imovel.html">Anunciar</a>
-                  <a href="#">A LocaHouse</a>
-                  <a href="cadastro.html" class="btn btn-back-green" id="btn-criar-conta">Criar conta</a>
-                  <a href="login.html" class="btn btn-outline-lightgreen" id="btn-login"><i class="bi bi-person-circle"></i> Entrar</a>
+                <div class="col-xs-12 col-sm-8 text-right">
+                    <a href="/">Home</a>
+                    <a href="imoveis.html">Alugar</a>
+                    <a href="cadastrar_imovel.html">Anunciar</a>
+                    <a href="#">A LocaHouse</a>
+                    <a href="cadastro.html" class="btn btn-back-green" id="btn-criar-conta">Criar conta</a>
+                    <a href="login.html" class="btn btn-outline-lightgreen" id="btn-login"><i class="bi bi-person-circle"></i> Entrar</a>
                 </div>
-              </div>`;
+              </div>
+              `;
     
     $('header').html(html);
 }
