487cbd28065076f8af2e716328c280aa63715cd6
Samuel Fernandes Monteiro <126695495+samuel-fm-coding@users.noreply.github.com>
2024-10-10 19:55:58 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -45,4 +45,23 @@ function viaCEP(cep, callback) {
   });
 }
 
+let slideIndex = 0;
+showSlides(slideIndex);
 
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    const slides = document.getElementsByClassName("slide");
+
+    if (n >= slides.length) {
+        slideIndex = 0;
+    } else if (n < 0) {
+        slideIndex = slides.length - 1;
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.transform = `translateX(-${slideIndex * 100}%)`;
+    }
+}
