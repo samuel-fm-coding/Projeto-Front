2c12d993809a5b856ce1ab290347ac11e1a87ec8
Samuel Fernandes Monteiro <126695495+samuel-fm-coding@users.noreply.github.com>
2024-11-12 22:11:23 -0300

    iniciando edição de user e carregamento de imoveis

--- js/main.js
+++ js/main.js
@@ -111,3 +111,15 @@ function showSlides(n) {
         slides[i].style.transform = `translateX(-${slideIndex * 100}%)`;
     }
 }
+
+function getQueryVariable(variable) {
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+    if (pair[0] == variable) {
+      return pair[1];
+    }
+  } 
+  alert('Parâmetro "'+variable+'" não encontrado');
+}
